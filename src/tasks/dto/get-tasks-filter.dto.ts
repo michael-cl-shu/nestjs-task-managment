9e4630408a7fb73f7e6fd1c9893ef26d8b4b79a7
Michael Shu <michael.shu@thermovisionusa.com>
2022-02-05 13:42:50 +0800

    Validation: search filter.

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,4 +1,11 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { TaskStatus } from '../task_model';
+
 export class GetTasksFilterDto {
-  status?: string;
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+  @IsOptional()
+  @IsString()
   search?: string;
 }
